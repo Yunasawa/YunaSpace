00f6933f867630a5617cddf0d1f7cd6a4dfd3aac
Yunasawa <yunasawa100@gmail.com>
2026-02-01 08:05:03 +0700

    [WIP]

--- project/documentation/script/card-handler.js
+++ project/documentation/script/card-handler.js
@@ -5,6 +5,7 @@ const project = params.get("p");
 const unitName = document.getElementById("unit-name");
 const iconImage = document.getElementById("icon-image");
 const googlePlay = document.getElementById("google-play");
+const githubRepo = document.getElementById("github-repo");
 
 if (!type || !project)
 {
@@ -24,6 +25,18 @@ fetch(jsonPath)
         unitName.textContent = data.name.toUpperCase();
         document.title = `${type.toUpperCase()}: ${data.name}`;
         iconImage.src = `../record/${type}/${project}/image/icon.png`;
+
+        if (data.github === "") {
+            githubRepo.lastChild.textContent = "PRIVATE";
+            githubRepo.classList.add("private");
+            githubRepo.classList.remove("primary");
+        }
+        else {
+            githubRepo.lastChild.textContent = "PUBLIC";
+            githubRepo.classList.remove("private");
+            githubRepo.classList.add("primary");
+            githubRepo.href = data.github;
+        }
 
         if (data.play === "") googlePlay.style.display = "none";
         else googlePlay.href = data.play;
